2d57f45b191e6d3d81830510598bed8430a13bfc
arthurfelipebr <43590411+arthurfelipebr@users.noreply.github.com>
2025-06-26 14:15:10 -0300

    teste

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from 'next/navigation';
-import { getUser } from '@/lib/auth';
-
-export default async function Home() {
-  const user = await getUser();
-  if (user) redirect('/dashboard');
-  else redirect('/login');
-}
